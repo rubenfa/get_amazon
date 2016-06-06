17661db4c9090620a9d8b4049f2130dbfbfc137d
rubenfa <ruben.fernandez.alfonso@outlook.com>
2016-06-06 22:56:19 +0200

    tests

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule GetAmazon.Mixfile do
      version: "0.0.1",
      elixir: "~> 1.2",
      #build_embedded: Mix.env == :prod,
-     #start_permanent: Mix.env == :prod,
+     #sotart_permanent: Mix.env == :prod,
      deps: deps]
   end
 
--- test/get_amazon_test.exs
+++ test/get_amazon_test.exs
@@ -2,7 +2,28 @@ defmodule GetAmazonTest do
   use ExUnit.Case
   doctest GetAmazon
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "all search has AWSAccessKeyId key" do
+    sut = GetAmazon.search([])
+    assert(String.contains? sut, "AWSAccessKeyId=")
   end
+
+  test "all search has AssociateTag key" do
+    sut = GetAmazon.search([])
+    assert(String.contains? sut, "AssociateTag=")
+  end
+
+  test "empty list of filters return url" do
+    sut = GetAmazon.search([])
+    assert(String.contains?(sut, "http://"))
+  end
+
+  test "returned url contains parameters like input filters" do
+    example_list = [{:SearchIndex, "Books"}, {:Keywords, "Matrix Revolutions"}]
+    sut = GetAmazon.search(example_list)
+
+    assert(String.contains?(sut, "SearchIndex=Books"))
+    assert(String.contains?(sut, "Keywords=Matrix Revolution"))
+
+  end
+  
 end
