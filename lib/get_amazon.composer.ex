2bc4ca4c55bc35c5508ab093e59e3d3e046233c8
rubenfa <ruben.fernandez.alfonso@gmail.com>
2016-06-08 18:02:49 +0200

    Changes not working already

--- lib/get_amazon.composer.ex
+++ lib/get_amazon.composer.ex
@@ -3,45 +3,58 @@ defmodule GetAmazon.Composer do
 
   @base_url Application.get_env(:amazon_conf, :APIBaseURL)
 
-  def compose_url(filters) do
+  def generate_url(filters) do
+    create_filter_list(filters)
+    |> add_mandatory_parameters
+   end
+
+  defp compose_url(filters) do
     add_security_keys(@base_url)
-    |> applyFilters filters_keys(@base_url)    
+    #|> applyFilters filters_keys(@base_url)    
   end
 
   defp add_mandatory_parameters(filters) do
-    [{:Service, Application.get_env(:amazon_conf, :APIService)}| filters]
+    [compose_value({:Service, Application.get_env(:amazon_conf, :APIService)}),
+     compose_value({:AWSAccesKeyId, Application.get_env(:amazon_conf, :AWSAccesKeyId)}),
+     compose_value({:AWSAccessKey, Application.get_env(:amazon_conf, :AWSAccessKey)}), 
+     compose_value({:AssociateTag, Application.get_env(:amazon_conf, :AssociateTag)})]
+    ++
+    (filters
+
   end
 
-  defp add_signature(filters) do
-
+  defp create_filter_list(parameters) do
+    filter_compose [], parameters
   end
 
-  defp filter_compose(result,[ {name, value} | tail]) do
-    filter_compose ( ["#{name}=#{value}" | result], head), tail)
+  defp compose_value({name, value}) do
+    "#{name}=#{value}"
   end
 
-  defp filter_compose(result, {name, value}) do
-     | result]
+  defp filter_compose(result,[ head | tail]) do
+    updated_result = [ compose_value(head) | result]
+
+    filter_compose updated_result ,  tail
   end
 
   defp filter_compose(result, []) do
     result
   end
 
-  defp applyFilters(url, [head | tail]) do
-    applyFilter head, applyFilters(url, tail)
-  end
+  # defp applyFilters(url, [head | tail]) do
+  #   applyFilter head, applyFilters(url, tail)
+  # end
 
-  defp applyFilters(url, []) do
-    url
-  end
+  # defp applyFilters(url, []) do
+  #   url
+  # end
 
-  defp applyFilter(url, {name, value}) do
-    url <> "&#{name}=#{value}"
-  end
+  # defp applyFilter(url, {name, value}) do
+  #   url <> "&#{name}=#{value}"
+  # end
 
-  # A unknown filter is ignored
-  defp applyFilter(url, _) do
-    url
-  end
+  # # A unknown filter is ignored
+  # defp applyFilter(url, _) do
+  #   url
+  # end
 end
