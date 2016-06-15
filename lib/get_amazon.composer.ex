53ae123e24be16ce6643f01d475d9798540f5e73
rubenfa <ruben.fernandez.alfonso@outlook.com>
2016-06-15 08:36:02 +0200

    working on composing Amazon parameters

--- lib/get_amazon.composer.ex
+++ lib/get_amazon.composer.ex
@@ -9,39 +9,52 @@ defmodule GetAmazon.Composer do
   end
 
   defp generate_query_parameters_list(search_parameters) do
-    create_parameter_list(search_parameters)
+    search_parameters
     |> add_mandatory_parameters
+    |> encode
+   # |> Enum.map (fn({k, v}) -> {k, URI.encode(v)} end)
   end
 
   defp add_mandatory_parameters(filters) do
-    timestamp = Time.get_utc_date_string()
+    timestamp = Time.get_current_utc_datestring() 
 
-
-    [compose_value({:Service, Application.get_env(:amazon_conf, :APIService)}),
-     compose_value({:AWSAccesKeyId, Application.get_env(:amazon_conf, :AWSAccesKeyId)}),
-     compose_value({:AWSAccessKey, Application.get_env(:amazon_conf, :AWSAccessKey)}), 
-     compose_value({:AssociateTag, Application.get_env(:amazon_conf, :AssociateTag)}),
-     compose_value({:Timestamp, timestamp })
+    [{:Service, Application.get_env(:amazon_conf, :APIService)},
+     {:AWSAccesKeyId, Application.get_env(:amazon_conf, :AWSAccesKeyId)},
+     {:AWSAccessKey, Application.get_env(:amazon_conf, :AWSAccessKey)}, 
+     {:AssociateTag, Application.get_env(:amazon_conf, :AssociateTag)},
+     {:Timestamp, timestamp }
     ]
     ++
     (filters)
   end
 
-  defp create_parameter_list(parameters) do
-    filter_compose [], parameters
+  # defp create_parameter_list(parameters) do
+  #   filter_compose [], parameters
+  # end
+
+  # defp compose_value({name, value}) do
+  #   "#{name}=#{value}"
+  # end
+
+  # defp compose_encoded_value({name, value}) do
+  #   encoded_value = URI.encode(value)
+
+  #   "#{name}=#{encoded_value}"
+  # end
+
+  defp encode(parameters) do
+    for {k, v} <- parameters, do: {k, URI.encode(v)}      
   end
 
-  defp compose_value({name, value}) do
-    "#{name}=#{value}"
-  end
 
-  defp filter_compose(result,[ head | tail]) do
-    updated_result = [ compose_value(head) | result]
-    filter_compose updated_result ,  tail
-  end
+  
+  # defp filter_compose(result,[ head | tail]) do
+  #   updated_result = [ compose_encoded_value(head) | result]
+  #   filter_compose updated_result ,  tail
+  # end
 
-  defp filter_compose(result, []) do
-    result
-  end
+  # defp filter_compose(result, []) do
+  #   result
+  # end
 
 end
