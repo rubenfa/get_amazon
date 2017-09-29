b44483d04126f398a70798236271ced89aa5708c
rubenfa <ruben.fernandez.alfonso@gmail.com>
2017-09-29 14:56:57 +0200

    Macro creation

---
+++ lib/schema/types.ex
@@ -0,0 +1,13 @@
+defmodule GetAmazon.Schema.Types do
+
+  defmacro type(:product, do: f)  do
+    f
+  end
+
+  defmacro field(identifier) do
+    identifier
+  end
+
+  
+
+end
---
+++ test/get_amazon_product_macro_test.exs
@@ -0,0 +1,51 @@
+defmodule GetAmazon.Tests.ProductMacro  do
+
+  use ExUnit.Case
+
+  import  GetAmazon.Schema.Types
+
+# alias GetAmazon.Schema.Types
+
+
+  test "Macro is loaded" do
+    assert Code.ensure_loaded?(GetAmazon.Schema.Types)
+  end
+
+  test "Product macro can assign value" do
+    
+    result =
+      type :product  do
+        "aas"
+    end
+
+    assert result == "aas"
+  end
+
+  test "Field macro can assign value" do
+    result = field :asin
+    assert result == :asin
+  end
+
+
+  test "Product macro is able to use one field" do
+    result =
+      type :product do
+        field :asin
+    end
+
+    assert result == :asin
+  end
+
+  test "Product macro is able to use more than one field" do
+    result =
+      type :product do
+      field :asin
+      field :name
+    end
+
+    assert result == [:asin, :name]
+  end
+
+
+
+end
