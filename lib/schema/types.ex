d26c7a4f536d68e1333341d3a3b7f320f15d97c1
ruben <contacto@charlascylon.com>
2017-10-07 00:46:53 +0200

    adding product macro

--- lib/schema/types.ex
+++ lib/schema/types.ex
@@ -1,13 +1,26 @@
 defmodule GetAmazon.Schema.Types do
 
-  defmacro type(:product, do: f)  do
-    f
-  end
+  defmacro product(do: block)  do
 
-  defmacro field(identifier) do
-    identifier
+    quote do
+      {:ok, var!(buffer, GetAmazon.Schema.Types) } = start_buffer([])
+      unquote(block)
+      result = get_buffer(var!(buffer, GetAmazon.Schema.Types))
+      stop_buffer(var!(buffer, GetAmazon.Schema.Types))
+      result
+    end
   end
 
   
+  defmacro field(name) do
+    quote do
+      put_buffer(var!(buffer, GetAmazon.Schema.Types), unquote(name))
+    end
+  end
 
-end
+  def start_buffer(state), do: Agent.start_link(fn -> state end)
+  def stop_buffer(buff), do: Agent.stop(buff)
+  def put_buffer(buff, content), do: Agent.update(buff, &[content | &1])
+  def get_buffer(buff), do: Agent.get(buff, &(&1)) |> Enum.reverse
+
+ end
--- test/get_amazon_product_macro_test.exs
+++ test/get_amazon_product_macro_test.exs
@@ -6,39 +6,23 @@ defmodule GetAmazon.Tests.ProductMacro  do
 
 # alias GetAmazon.Schema.Types
 
-
   test "Macro is loaded" do
-    assert Code.ensure_loaded?(GetAmazon.Schema.Types)
-  end
-
-  test "Product macro can assign value" do
-    
-    result =
-      type :product  do
-        "aas"
-    end
-
-    assert result == "aas"
-  end
-
-  test "Field macro can assign value" do
-    result = field :asin
-    assert result == :asin
+     assert Code.ensure_loaded?(GetAmazon.Schema.Types)
   end
 
 
-  test "Product macro is able to use one field" do
-    result =
-      type :product do
-        field :asin
-    end
+   test "Product macro is able to use one field" do
+     result =
+       product do
+         field :asin
+     end
 
-    assert result == :asin
-  end
+     assert result == [:asin]
+   end
 
   test "Product macro is able to use more than one field" do
     result =
-      type :product do
+     product do
       field :asin
       field :name
     end
@@ -47,5 +31,6 @@ defmodule GetAmazon.Tests.ProductMacro  do
   end
 
 
+  
 
 end
