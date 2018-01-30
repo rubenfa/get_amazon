3bbfdc95f0383207d089042ecf14e0ca93ac2898
ruben <contacto@charlascylon.com>
2018-01-30 06:55:12 +0100

    Changes to importing SweetXml structure from txt file

--- lib/get_amazon.macro.get_info.ex
+++ lib/get_amazon.macro.get_info.ex
@@ -1,7 +1,65 @@
 defmodule GetAmazon.GetInfo do
+  import SweetXml
+
+  @file_xpaths "/schemas/item_search_request.txt"
+
   @moduledoc """
-  **This macro is working in progress**. I'm trying to provide a DSL to make petitions to Amazon APIO
+  **This macro is working in progress**. I'm trying to provide a DSL to make petitions to Amazon API
   """
+
+  defmacro __using__() do
+    quote do
+      GetAmazon.GetInfo.load_xpaths()
+    end
+  end
+
+  def load_xpaths(file_name) do
+
+    xpaths =load_from_file(file_name)
+
+    IO.inspect(xpaths)
+
+    xpaths
+    |> Enum.reverse
+    |> Enum.reduce([], fn(x, acc) -> create_sigil_call(x, acc) end)    
+  end
+
+
+  def create_sigil_call({"NODE", parent, name, xpath}, acc) do
+    element =[parent, [ { String.to_atom(name), xpath}]]
+    [element | acc]
+  end
+
+  def create_sigil_call({"LEAF", parent, name, xpath}, acc) do
+    
+
+    ["a" | acc]
+  end
+
+  def load_from_file(file_name) do
+    File.stream!(Path.join([__DIR__, file_name]))
+    |> Stream.map(&String.trim/1)
+    |> Enum.to_list()
+    |> Enum.map(fn line -> line |> String.split(";") |> List.to_tuple() end)
+    |> Enum.map(fn {node_type, parent, parent_type, name, xpath, xpath_type} ->
+      {node_type, create_sweet_sigil(parent, parent_type), name,
+       create_sweet_sigil(xpath, xpath_type)}
+    end)
+  end
+
+  def create_sweet_sigil(parent, "") do
+    SweetXml.sigil_x(parent)
+  end
+
+  def create_sweet_sigil(parent, type) do
+    sweet_type = type |> to_charlist |> create_sweet_type
+    SweetXml.sigil_x(parent, sweet_type)
+  end
+
+  def create_sweet_type(type) do
+    type
+    |> Enum.uniq()
+  end
 
   defmacro amazon_type(type, xml, do: get_block) do
     quote do
--- test/get_amazon_dsl_macro.exs
+++ test/get_amazon_dsl_macro.exs
@@ -4,6 +4,8 @@ defmodule GetAmazon.GetInfoMacro do
 
   import GetAmazon.GetInfo
 
+  alias GetAmazon.GetInfo
+
   """
   amazon_type xml do
     get :asin
@@ -20,8 +22,8 @@ defmodule GetAmazon.GetInfoMacro do
   test "macro using get" do
     result =
       amazon_type :book, "" do
-      get(:asin)
-      get(:title)
+        get(:asin)
+        get(:title)
       end
 
     assert result == %{:asin => 1234, :title => "Book title"}
@@ -35,4 +37,141 @@ defmodule GetAmazon.GetInfoMacro do
     end
   end
 
+  test "Testing SweetXml creation types" do
+    result = 'sl' |> GetInfo.create_sweet_type()
+    assert result == 'sl'
+  end
+
+  test "Testing SweetXml creation types with duplicates " do
+    result = 'sFFl' |> GetInfo.create_sweet_type()
+    assert result == 'sFl'
+  end
+
+  test "Testing SweetXml sigil creation without types" do
+    result = GetInfo.create_sweet_sigil("\\Items", "")
+
+    assert result == %SweetXpath{
+             path: '\\Items'
+           }
+  end
+
+  test "Testing SweetXml sigil creation with type l" do
+    result = GetInfo.create_sweet_sigil("\\Items", "l")
+    IO.inspect(result)
+
+    assert result == %SweetXpath{
+             path: '\\Items',
+             is_list: true
+           }
+  end
+
+  test "Testing SweetXml sigil creation with type ls" do
+    result = GetInfo.create_sweet_sigil("\\Items", "ls")
+    IO.inspect(result)
+
+    assert result == %SweetXpath{
+             path: '\\Items',
+             is_list: true,
+             cast_to: :string
+           }
+  end
+
+  test "Testing SweetXml sigil creation with type lS" do
+    result = GetInfo.create_sweet_sigil("\\Items", "lS")
+    IO.inspect(result)
+
+    assert result == %SweetXpath{
+             path: '\\Items',
+             is_list: true,
+             cast_to: :soft_string
+           }
+  end
+
+  test "Loading file with SweetXml expressions" do
+    result = GetInfo.load_from_file("../test/schemas/test_item_search_request.txt")
+
+    assert result == [
+             {"NODE",
+              %SweetXpath{
+                cast_to: false,
+                is_keyword: false,
+                is_list: true,
+                is_optional: false,
+                is_value: true,
+                namespaces: [],
+                path: '//Items',
+                transform_fun: &SweetXpath.Priv.self_val/1
+              }, "Items",
+              %SweetXpath{
+                cast_to: false,
+                is_keyword: false,
+                is_list: false,
+                is_optional: false,
+                is_value: true,
+                namespaces: [],
+                path: './Item',
+                transform_fun: &SweetXpath.Priv.self_val/1
+              }},
+             {"LEAF",
+              %SweetXpath{
+                cast_to: false,
+                is_keyword: false,
+                is_list: true,
+                is_optional: false,
+                is_value: true,
+                namespaces: [],
+                path: './Item',
+                transform_fun: &SweetXpath.Priv.self_val/1
+              }, "asin",
+              %SweetXpath{
+                cast_to: :string,
+                is_keyword: false,
+                is_list: false,
+                is_optional: false,
+                is_value: true,
+                namespaces: [],
+                path: './ASIN/text()',
+                transform_fun: &SweetXpath.Priv.self_val/1
+              }},
+             {"LEAF",
+              %SweetXpath{
+                cast_to: false,
+                is_keyword: false,
+                is_list: true,
+                is_optional: false,
+                is_value: true,
+                namespaces: [],
+                path: './Item',
+                transform_fun: &SweetXpath.Priv.self_val/1
+              }, "detail_page",
+              %SweetXpath{
+                cast_to: :string,
+                is_keyword: false,
+                is_list: false,
+                is_optional: false,
+                is_value: true,
+                namespaces: [],
+                path: './DetailPage/text()',
+                transform_fun: &SweetXpath.Priv.self_val/1
+              }}
+           ]
+  end
+
+
+  test "Schema readed has to be converted to structure" do
+    result = GetInfo.load_xpaths("../test/schemas/test_item_search_request.txt")
+
+    assert result == []
+  end
+
+  # ROOT;~x"//Items"
+  # Items;~x"./Item"l
+  # asin;~x"./ASIN/text()"s
+  # detail_page;~x"./DetailPage/text()"s
+
+  test "Macro load xpaths from file" do
+    xpaths = GetAmazon.GetInfo.load_xpaths()
+
+    assert xpaths == []
+  end
 end
